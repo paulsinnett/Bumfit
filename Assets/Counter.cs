e3cec99d4a877c9a2da4bc78436fe183c56e7a18
Paul Sinnett <paul.sinnett@gmail.com>
2015-02-10 15:32:40 +0000

    Week 3 example

---
+++ Assets/Counter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Counter : MonoBehaviour {
+
+	Text counter;
+	int count = 0;
+
+	public string[] numbers;
+
+	// Use this for initialization
+	void Start () {
+	
+		counter = GetComponent<Text> ();
+		counter.color = Color.white;
+	}
+
+	public void AddOneSheep()
+	{
+		count++;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		int stones = count / 10;
+
+		counter.text = string.Format ("{0} stones plus {1}", stones, numbers[count % 10]);
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		text.color = Color.white;
+		//Time.timeScale = 30f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		int minutes = (int)Time.time / 60;
+		int seconds = (int)Time.time % 60;
+
+		text.text = string.Format ("{0:D2}:{1:D2}", minutes, seconds);
+	}
+}
